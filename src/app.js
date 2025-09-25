98e3fd16ad5fd0575c609728f5acc0081cea7c24
George-Kibe <georgekibewambui@gmail.com>
2025-09-25 20:37:55 +0300

    configuring eslint and prettier

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req,res) => {
+  res.json({
+    message: "hello from a container: Acquisitions API",
+    service: "hello Node",
+    pod: process.env.POD_NAME || "Not specified",
+    time:  new Date().toISOString(),
+  });
+});
+
+app.get("/ready", (req, res) => {
+  res.json({
+    message: "Application is ready, up and running",
+    service: "hello Node",
+    status: 200,
+    pod: process.env.POD_NAME || "Not specified",
+    time:  new Date().toISOString(),
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    message: "Application health status is OKAY",
+    status: 200,
+    time:  new Date().toISOString(),
+  });
+});
+
+export default app; 
