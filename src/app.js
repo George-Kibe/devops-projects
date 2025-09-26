326d652aba47ed19f2ef93fce49753192922d5d4
George-Kibe <georgekibewambui@gmail.com>
2025-09-26 17:14:30 +0300

    tests with jest

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "#routes/auth.routes.js";
+import usersRoutes from "#routes/users.routes.js";
 import { securityMiddleware } from "#middlewares/security.middleware.js";
 
 // console.log("Database URL: ", process.env.DATABASE_URL);
@@ -42,11 +43,19 @@ app.get("/ready", (req, res) => {
 app.get("/health", (req, res) => {
   res.status(200).json({
     message: "Application health status is OKAY",
-    status: 200,
+    status: "OK",
     uptime: process.uptime(),
-    time: new Date().toISOString(),
+    timestamp: new Date().toISOString(),
   });
+});
+app.get("/api", (req, res) => {
+  res.status(200).json({ message: "Acquisitions API is running!" });
 });
 
 app.use("/api/auth", authRoutes );
+app.use("/api/users", usersRoutes);
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
 export default app;
